fdfecc557d6178df4501b0451072a9ac6d733128
tim.marks <timmarks90@gmail.com>
2021-07-25 17:04:10 -0700

    minimal home

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h1>
+          Tim Marks
+        </h1>
       </header>
     </div>
   );
